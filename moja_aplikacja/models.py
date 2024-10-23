c0551310b7f95b4b81a6b9e0880301c210efb22c
Otlos2002 <szymon.otlowski1@gmail.com>
2024-10-23 16:31:23 +0200

    Zatwierdzenie wszystkich zmian w branch lab_3

--- moja_aplikacja/models.py
+++ moja_aplikacja/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+# from django.utils import timezone
 
-# Create your models here.
+class Stanowisko(models.Model):
+    nazwa = models.CharField(max_length=100)  # Pole tekstowe, wymagane, niepuste
+    opis = models.TextField(blank=True, null=True)  # Pole tekstowe, opcjonalne
+
+    def __str__(self):
+        return self.nazwa
+
+
+class Osoba(models.Model):
+    class Plec(models.IntegerChoices):
+        KOBIETA = 1, 'Kobieta'
+        MEZCZYZNA = 2, 'Mężczyzna'
+        INNE = 3, 'Inne'
+
+    imie = models.CharField(max_length=100, blank=False, null=False)
+    nazwisko = models.CharField(max_length=100, blank=False, null=False)
+    plec = models.IntegerField(choices=Plec.choices, blank=False, null=False)  # Użycie IntegerChoices
+    stanowisko = models.ForeignKey('Stanowisko', on_delete=models.CASCADE)
+    data_dodania = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.imie} {self.nazwisko}"
+
+    class Meta:
+        ordering = ['nazwisko']
