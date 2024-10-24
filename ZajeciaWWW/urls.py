12fe2c3a0148bc3dea18d439f06a9fa6d7a02887
Otlos2002 <szymon.otlowski1@gmail.com>
2024-10-24 14:13:25 +0200

    zrobienie zadan 5

--- ZajeciaWWW/urls.py
+++ ZajeciaWWW/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('osoby/', views.get_osoby, name='get_osoby'),
     path('osoby/create/', views.create_osoba, name='create_osoba'),
+    path('stanowiska/', views.get_stanowiska, name='get_stanowiska'),
+    path('stanowiska/create/', views.create_stanowisko, name='create_stanowisko'),
 ] + debug_toolbar_urls()
 
 
--- moja_aplikacja/views.py
+++ moja_aplikacja/views.py
@@ -1,20 +1,32 @@
-from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from .models import Osoba
-from .serializers import OsobaSerializer
+from .models import Osoba, Stanowisko
+from .serializers import OsobaModelSerializer, StanowiskoModelSerializer
 
 @api_view(['GET'])
 def get_osoby(request):
     osoby = Osoba.objects.all()
-    serializer = OsobaSerializer(osoby, many=True)
+    serializer = OsobaModelSerializer(osoby, many=True)
     return Response(serializer.data)
 
 @api_view(['POST'])
 def create_osoba(request):
-    serializer = OsobaSerializer(data=request.data)
+    serializer = OsobaModelSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
         return Response(serializer.data)
     return Response(serializer.errors, status=400)
 
+@api_view(['GET'])
+def get_stanowiska(request):
+    stanowiska = Stanowisko.objects.all()
+    serializer = StanowiskoModelSerializer(stanowiska, many=True)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def create_stanowisko(request):
+    serializer = StanowiskoModelSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors, status=400)
